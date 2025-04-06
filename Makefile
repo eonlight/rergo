9b359d6a4dd7f9fa90682d193112dddb0a1a04e4
eonlight <3688763+eonlight@users.noreply.github.com>
2025-04-06 15:18:32 +0100

    updated design

--- Makefile
+++ Makefile
@@ -5,6 +5,11 @@ build:
 	ergogen . || unlink config.yaml
 	unlink config.yaml 2> /dev/null
 
+case:
+	@echo Go To https://3d.hrg.hr/jscad/V1/
+	@echo Drop the jscad file on bototm left
+	@echo Download STL file
+
 clean:
 	rm -rf output 2> /dev/null
 	unlink config.yaml 2> /dev/null
--- footprints/b3u1000p.js
+++ footprints/b3u1000p.js
@@ -1,16 +1,14 @@
 module.exports = {
-    nets: {
-        r1: 'r1',
-        r2: 'r2'
-    },
-    params: {
-        class: 'S',
-	reverse: true
-    },
-    body: p => {
-	    const standard = `
+	params: {
+		class: 'S',
+		reverse: true,
+		r1: undefined,
+		r2: undefined
+	},
+	body: p => {
+		const standard = `
 	(module Button_Switch_SMD:SW_SPST_B3U-1000P (layer F.Cu) (tedit 5A02FC95)
-        ${p.at /* parametric position */}
+		${p.at /* parametric position */}
 	  (descr "Ultra-small-sized Tactile Switch with High Contact Reliability, Top-actuated Model, without Ground Terminal, without Boss")
 	  (tags "Tactile Switch")
 	  (attr smd)
@@ -24,28 +22,28 @@ module.exports = {
 	  (fp_line (start 2.4 1.65) (end 2.4 -1.65) (layer F.CrtYd) (width 0.05))
 	  (fp_line (start -2.4 1.65) (end 2.4 1.65) (layer F.CrtYd) (width 0.05))
 	  (fp_text user %R (at 0 -2.5) (layer F.Fab)
-	    (effects (font (size 1 1) (thickness 0.15)))
+		(effects (font (size 1 1) (thickness 0.15)))
 	  )
+		`
 
-	      `
-	    function pins(def_neg, def_pos, def_side) {
+		function pins(def_neg, def_pos, def_side) {
 		  return `
-		    ${''/* pins */}
-	  (pad 1 smd rect (at ${def_neg}1.7 0) (size 0.9 1.7) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask) ${p.net.r1.str})
-	  (pad 2 smd rect (at ${def_pos}1.7 0) (size 0.9 1.7) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask) ${p.net.r2.str})
+			${''/* pins */}
+	  (pad 1 smd rect (at ${def_neg}1.7 0) (size 0.9 1.7) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask) ${p.r1.str})
+	  (pad 2 smd rect (at ${def_pos}1.7 0) (size 0.9 1.7) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask) ${p.r2.str})
 		  `
-	    }
-	    if(p.param.reverse) {
-	      return `
+		}
+		if(p.reverse) {
+		  return `
 		${standard}
 		${pins('-', '', 'B')}
 		${pins('', '-', 'F')})
 		`
-	    } else {
-	      return `
+		} else {
+		  return `
 		${standard}
 		${pins('-', '', 'B')})
 		`
-	    }
+		}
 	}
 }
